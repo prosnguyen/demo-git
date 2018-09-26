3073fb75e346d66484426b7cbb9ebf2aa753253e
Cuong <vietcuong0409@mail.com>
2018-09-27 01:09:13 +0700

    commit lan 2

---
+++ app/src/main/java/com/example/cuong/mygit/Module1/Test1Activity.java
@@ -0,0 +1,15 @@
+package com.example.cuong.mygit.Module1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cuong.mygit.R;
+
+public class Test1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test1);
+    }
+}
